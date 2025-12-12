c4596232dda0fb48090c21151618b483d8a3574e
saberpourHCI <saberpour@cs.uni-saarland.de>
2025-12-12 18:03:39 +0100

    Gif updated

--- py/make_gif.py
+++ py/make_gif.py
@@ -5,4 +5,4 @@ frames = sorted(glob.glob("../c/code/png_frames/frame_*.png"))
 
 images = [imageio.imread(f) for f in frames]
 
-imageio.mimsave("training_progress.gif", images, duration=0.2)
+imageio.mimsave("training_progress.gif", images, duration=0.2, loop=0)
